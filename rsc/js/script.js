8d9b64646cc800915c4b5f81f0000ed5143fa319
Steve <steve.gitprofile@outlook.com>
2024-03-29 22:51:03 +0200

    implemented login feature

--- rsc/js/script.js
+++ rsc/js/script.js
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
+// EASYBANK APP
 
 // Data
 const account1 = {
@@ -35,7 +35,12 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
+let activeAccount;
+// ==========================================================================
 // Elements
+// ==========================================================================
+
+// label elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
@@ -44,15 +49,18 @@ const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
 const labelTimer = document.querySelector('.timer');
 
+// container elements
 const containerApp = document.querySelector('.app');
 const containerTransactions = document.querySelector('.movements');
 
+// buttons
 const btnLogin = document.querySelector('.login__btn');
 const btnTransfer = document.querySelector('.form__btn--transfer');
 const btnLoan = document.querySelector('.form__btn--loan');
 const btnClose = document.querySelector('.form__btn--close');
 const btnSort = document.querySelector('.btn--sort');
 
+// input elements
 const inputLoginUsername = document.querySelector('.login__input--user');
 const inputLoginPin = document.querySelector('.login__input--pin');
 const inputTransferTo = document.querySelector('.form__input--to');
@@ -61,6 +69,9 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// ==========================================================================
+// Functions
+// ==========================================================================
 function displayTransactions(transactionsArr) {
   let withdrawalNumber = 1;
   let depositNumber = 1;
@@ -101,16 +112,16 @@ function calcDisplayBalance(transactions) {
   labelBalance.textContent = `${balance} ZAR`;
 }
 
-function calcDisplaySummary(transactions, interestRate) {
-  const depositsTotal = transactions
+function calcDisplaySummary(account) {
+  const depositsTotal = account.transactions
     .filter(transac => transac > 0)
     .reduce((acc, transac) => acc + transac);
-  const withdrawalsTotal = transactions
+  const withdrawalsTotal = account.transactions
     .filter(transac => transac < 0)
     .reduce((acc, transac) => acc + transac);
-  const interestTotal = transactions
+  const interestTotal = account.transactions
     .filter(transac => transac > 0)
-    .map(deposit => (deposit * interestRate) / 100)
+    .map(deposit => (deposit * account.interestRate) / 100)
     .filter(interestPayment => interestPayment > 1) // bank not pay interest on cents
     .reduce((acc, interest) => acc + interest);
 
@@ -119,7 +130,30 @@ function calcDisplaySummary(transactions, interestRate) {
   labelSumInterest.textContent = `R ${interestTotal}`;
 }
 
+// ==========================================================================
+// Event Handlers
+// ==========================================================================
+
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+  activeAccount = accounts.find(
+    account => account.username === inputLoginUsername.value
+  );
+  if (activeAccount?.pin === +inputLoginPin.value) {
+    // display welcome message
+    labelWelcome.textContent = `Hi ${activeAccount.owner}`;
+    // display transactions
+    displayTransactions(activeAccount.transactions);
+    // display balance
+    calcDisplayBalance(activeAccount.transactions);
+    // display summary
+    calcDisplaySummary(activeAccount);
+    // show app & clear input fields
+    containerApp.style.opacity = 100;
+    inputLoginPin.value = '';
+    inputLoginUsername.value = '';
+    inputLoginPin.blur(); // remove focus
+  }
+});
+
 createUsernames(accounts);
-displayTransactions(account1.transactions);
-calcDisplayBalance(account1.transactions);
-calcDisplaySummary(account1.transactions, account1.interestRate);
