e6813598882768d3fbbe1417390bafb3affde9e6
Steve <steve.gitprofile@outlook.com>
2024-03-31 12:33:42 +0200

    added dates rendering and fixed bug for displaying other users transactions after follow up login

--- rsc/js/script.js
+++ rsc/js/script.js
@@ -7,9 +7,22 @@
 // Data
 const account1 = {
   owner: 'Steven OEhley',
-  transactions: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  transactions: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  transactionDates: [
+    '2022-11-18T21:31:17.178Z',
+    '2022-12-23T07:42:02.383Z',
+    '2023-01-28T09:15:04.904Z',
+    '2023-04-01T10:17:24.185Z',
+    '2023-05-08T14:11:59.604Z',
+    '2023-07-26T17:01:17.194Z',
+    '2023-07-28T23:36:17.929Z',
+    '2023-08-01T10:51:36.790Z',
+  ],
+  currency: 'ZAR',
+  locale: 'en-GB',
 };
 
 const account2 = {
@@ -17,6 +30,19 @@ const account2 = {
   transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  transactionDates: [
+    '2022-11-01T13:15:33.035Z',
+    '2022-11-30T09:48:16.867Z',
+    '2022-12-25T06:04:23.907Z',
+    '2023-01-25T14:18:46.235Z',
+    '2023-02-05T16:33:06.386Z',
+    '2023-04-10T14:43:26.374Z',
+    '2023-06-25T18:49:59.371Z',
+    '2023-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -24,6 +50,18 @@ const account3 = {
   transactions: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  transactionDates: [
+    '2022-11-01T13:15:33.035Z',
+    '2022-11-30T09:48:16.867Z',
+    '2022-12-25T06:04:23.907Z',
+    '2023-01-25T14:18:46.235Z',
+    '2023-02-05T16:33:06.386Z',
+    '2023-04-10T14:43:26.374Z',
+    '2023-06-25T18:49:59.371Z',
+    '2023-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account4 = {
@@ -31,11 +69,20 @@ const account4 = {
   transactions: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  transactionDates: [
+    '2022-11-01T13:15:33.035Z',
+    '2022-11-30T09:48:16.867Z',
+    '2022-12-25T06:04:23.907Z',
+    '2023-01-25T14:18:46.235Z',
+  ],
+  currency: 'USD',
+  locale: 'en-GB',
 };
 
 const accounts = [account1, account2, account3, account4];
 
 let activeAccount;
+let sortedDisplay = false;
 // ==========================================================================
 // Elements
 // ==========================================================================
@@ -75,10 +122,24 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // ==========================================================================
 // Functions
 // ==========================================================================
-function displayTransactions(transactionsArr) {
+function displayTransactions(account, sort = false) {
   let withdrawalNumber = 1;
   let depositNumber = 1;
-  transactionsArr.forEach((transaction, index) => {
+
+  // prevent users from seeing others transactions if log in one after the other
+  containerTransactions.innerHTML = '';
+
+  const transactions = sort
+    ? [...account.transactions].sort((a, b) => a - b)
+    : account.transactions;
+
+  transactions.forEach((transaction, index) => {
+    const transactionDate = new Date(activeAccount.transactionDates[index]);
+    const day = `${transactionDate.getDate()}`.padStart(2, 0); // padStart will insert a zero if value less than 2 in length
+    const month = `${transactionDate.getMonth() + 1}`.padStart(2, 0); // month are zero indexed add one then padStart ie 3 will be 03
+    const year = transactionDate.getFullYear();
+    const dateToRender = `${day}/${month}/${year}`;
+
     const transactionType = transaction > 0 ? 'deposit' : 'withdrawal';
     const htmlToRender = `
     <div class="movements__row">
@@ -86,9 +147,12 @@ function displayTransactions(transactionsArr) {
       index + 1
     } ${transactionType} #${
       transactionType === 'deposit' ? depositNumber : withdrawalNumber
-    }</div>
+    }     </div>
+          <div class="movements__date">${dateToRender}</div>       
  
-          <div class="movements__value movements__value--${transactionType}">R ${transaction}
+          <div class="movements__value movements__value--${transactionType}">R ${transaction.toFixed(
+      2
+    )}
           </div>
         </div>
     `;
@@ -108,13 +172,27 @@ function createUsernames(accs) {
   });
 }
 
+function createDate(time = true) {
+  const currentDate = new Date();
+  // want - day / month / year
+  const day = `${currentDate.getDate()}`.padStart(2, 0); // padStart will insert a zero if value less than 2 in length
+  const month = `${currentDate.getMonth()}`.padStart(2, 0); // month are zero indexed add one then padStart ie 3 will be 03
+  const year = currentDate.getFullYear();
+  const hour = currentDate.getHours();
+  const minute = currentDate.getMinutes();
+  const dateToRender = time
+    ? `${day}/${month}/${year}, ${hour}:${minute}`
+    : `${day}/${month}/${year}`;
+  return dateToRender;
+}
+
 function calcDisplayBalance(account) {
   const balance = account.transactions.reduce(
     (acc, transaction) => acc + transaction,
     0
   );
   account.balance = balance;
-  labelBalance.textContent = `${balance} ZAR`;
+  labelBalance.textContent = `${balance.toFixed(2)} ZAR`;
 }
 
 function calcDisplaySummary(account) {
@@ -130,14 +208,14 @@ function calcDisplaySummary(account) {
     .filter(interestPayment => interestPayment > 1) // bank not pay interest on cents
     .reduce((acc, interest) => acc + interest);
 
-  labelSumIn.textContent = `R ${depositsTotal}`;
-  labelSumOut.textContent = `R ${Math.abs(withdrawalsTotal)}`;
-  labelSumInterest.textContent = `R ${interestTotal}`;
+  labelSumIn.textContent = `R ${depositsTotal.toFixed(2)}`;
+  labelSumOut.textContent = `R ${Math.abs(withdrawalsTotal).toFixed(2)}`;
+  labelSumInterest.textContent = `R ${interestTotal.toFixed(2)}`;
 }
 
 function updateUIForAccount() {
   // display transactions
-  displayTransactions(activeAccount.transactions);
+  displayTransactions(activeAccount);
   // display balance
   calcDisplayBalance(activeAccount);
   // display summary
@@ -157,8 +235,13 @@ btnLogin.addEventListener('click', e => {
   if (activeAccount?.pin === +inputLoginPin.value) {
     // display welcome message
     labelWelcome.textContent = `Hi ${activeAccount.owner}`;
+    // update UI with account info
     updateUIForAccount();
+    //set date before showing app
+    labelDate.textContent = createDate(true);
+    // show app
     containerApp.style.opacity = 100;
+    // reset login form / information
     inputLoginPin.value = '';
     inputLoginUsername.value = '';
     inputLoginPin.blur(); // remove focus
@@ -184,8 +267,12 @@ btnTransfer.addEventListener('click', e => {
     transferAccount?.username !== activeAccount.username
   ) {
     // transfer money
+    const dateToPush = new Date().toISOString();
+
     activeAccount.transactions.push(-transferAmount);
     transferAccount.transactions.push(transferAmount);
+    activeAccount.transactionDates.push(dateToPush);
+    transferAccount.transactionDates.push(dateToPush);
     updateUIForAccount();
     inputTransferTo.value = '';
     inputTransferAmount.value = '';
@@ -203,14 +290,16 @@ btnTransfer.addEventListener('click', e => {
 
 btnLoan.addEventListener('click', e => {
   e.preventDefault();
-  const loanRequest = +inputLoanAmount.value;
+  const loanRequest = Math.floor(inputLoanAmount.value); // Math.floor() does type coercion no need to convert
   // only grant loan if at least one deposit is >= 10% of loan request
   if (
     activeAccount.transactions.some(
       transaction => transaction >= loanRequest * 0.1
     )
   ) {
+    const dateToPush = new Date().toISOString();
     activeAccount.transactions.push(loanRequest);
+    activeAccount.transactionDates.push(dateToPush);
     updateUIForAccount();
     inputLoanAmount.value = '';
     inputLoanAmount.blur();
@@ -255,6 +344,12 @@ btnClose.addEventListener('click', e => {
   inputCloseUsername.value = '';
   inputClosePin.value = '';
   inputClosePin.blur();
+});
+
+btnSort.addEventListener('click', e => {
+  e.preventDefault();
+  sortedDisplay = !sortedDisplay;
+  displayTransactions(activeAccount, sortedDisplay);
 });
 
 // ==========================================================================
